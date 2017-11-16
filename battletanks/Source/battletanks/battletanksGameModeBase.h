8a03aec712c5513e631516de738f67226b634c18
LAPTOP-9BANSJ6V\walla <wallac132n@gmail.com>
2017-11-16 18:46:32 -0500

    gitignore update

---
+++ battletanks/Source/battletanks/battletanksGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "battletanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKS_API AbattletanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
